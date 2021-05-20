4473a281b550573c4bc74efc5a85df98c2f59f20
Knarkzel <knarkzel@gmail.com>
2021-05-21 00:02:06 +0200

    added pad

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,6 @@
 
 extern crate alloc;
 use ogc::prelude::*;
-use ogc::prelude::ogc_sys as sys;
-use ogc::mem_cached_to_uncached;
-
-const FIFO_SIZE: usize = 256 * 1024;
 
 #[start]
 fn main(_argc: isize, _argv: *const *const u8) -> isize {
@@ -31,26 +27,15 @@ fn main(_argc: isize, _argv: *const *const u8) -> isize {
     // Wait for Video setup to complete.
     Video::wait_vsync();
 
-    // Other
-    let background = sys::_gx_color { r: 0, g: 0, b: 0, a: 0 };
-    let mut config = Video::get_preferred_mode();
-
-    unsafe {
-        let fifo_buffer = mem_cached_to_uncached!(libc::memalign(32, FIFO_SIZE));
-        libc::memset(fifo_buffer, 0, FIFO_SIZE);
-        sys::GX_Init(fifo_buffer, FIFO_SIZE as u32);
-        sys::GX_SetCopyClear(background, 0x00FFFFFF);
-        sys::GX_SetViewport(0.0, 0.0, config.framebuffer_width as f32, config.embed_framebuffer_height as f32, 0.0, 1.0);
-        sys::GX_SetDispCopyYScale((config.extern_framebuffer_height as f32) / (config.framebuffer_width as f32));
-        sys::GX_SetScissor(0, 0, config.framebuffer_width.into(), config.embed_framebuffer_height.into());
-        sys::GX_SetDispCopySrc(0, 0, config.framebuffer_width, config.embed_framebuffer_height);
-        sys::GX_SetDispCopyDst(config.framebuffer_width, config.extern_framebuffer_height);
-        sys::GX_SetCopyFilter(config.anti_aliasing, &mut config.sample_pattern[0], sys::GX_TRUE as u8, &mut config.v_filter[0]);
-        sys::GX_SetFieldMode(config.field_rendering, sys::GX_ENABLE as u8);
-    }
+    // Input
+    let pad = Pad::init();
 
     loop {
         // Wait for the next frame.
         Video::wait_vsync();
+
+        pad.scan_pads();
+
+        println!("L: {}, R: {}", pad.trigger_l(Controller::One), pad.trigger_r(Controller::One));
     }
 }
